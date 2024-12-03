19ba8c17116c6ecf505ddb32d42bc5bad97efda9
Gabriela donato <146032843+GabsDonato@users.noreply.github.com>
2024-12-03 19:36:07 -0300

    formulario criando tabela

--- projeto-final/src/Formulario.js
+++ projeto-final/src/Formulario.js
@@ -10,10 +10,14 @@ class Formulario extends React.Component{
     //render
     render(){
         return(
-            <form>
-                <input type='text' placeholder='informe o nome' className='form-control' />
-                <input type='number' placeholder='informe a idade' className='form-control' />
-                <input type='submit' value='Cadastrar' className='btn btn-primary'/>
+            <form onSubmit={this.props.funcaoBotao} >
+
+                <input type='text' placeholder='informe o nome' className='form-control' name='nome  onChange={this.props.funcaoCampo}  />
+
+                <input type='number' placeholder='informe a idade' className='form-control' name='idade' onChange={this.props.funcaoCampo}  />
+
+                <input type='submit' value='Cadastrar' className='btn btn-primary' />
+
             </form>
         );
     }
--- projeto-final/src/index.js
+++ projeto-final/src/index.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import '../src/Estilos.css';
-
+import './Estilos.css';
+import Tabela from './Tabela';
 import Formulario from './Formulario';
 
 
 
 class Principal extends React.Component{
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            nome: '',
+            idaide: null
+        }
+
+    }
+
+    // onChange ao digitar
+
+    aoDigitar = (campo) => {
+        this.setState({[campo.target.name] : campo.target.value});
+    }
+
+    aoClicar = (botao) => {
+        botao.preventDefault();
+        console.log(this.state);
+    }
+
 
     render(){
         return(
             <div>
-                <Formulario />
+                <Formulario funcaoBotao={this.aoClicar}  funcaoCampo={this.aoDigitar}/>
+                <Tabela />
             </div>
         );
     }
